8dfc91eac7ef69aadafd07936fa5f3eaadd881af
tsavou <theosavourat@gmail.com>
2025-03-10 15:46:44 +0100

    feat(calculatrice): add calculation methods
    - Added `add` method to perform addition.
    - Added `subtract` method to perform subtraction.
    - Added `multiply` method to perform multiplication.

---
+++ src/calculatrice.ts
@@ -0,0 +1,19 @@
+export class Calculatrice {
+
+
+    add(a: number, b: number): number {
+        const result = a + b;
+        return result;
+    }
+
+
+    subtract(a: number, b: number) {
+        const result = a - b;
+        return result;
+    }
+
+    multiply(a: number, b: number) {
+        const result = a * b;
+        return result;
+    }
+}
